b3df3945b3033832286842dfc4b58485c9352edf
AndresFLK <andrescrptycol@gmail.com>
2023-02-01 21:44:38 -0600

    Tarea2

---
+++ Tarea2/delangmedical/src/main/java/delangmedical/delangmedical/DelangmedicalApplication.java
@@ -0,0 +1,13 @@
+package delangmedical.delangmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelangmedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelangmedicalApplication.class, args);
+	}
+
+}
---
+++ Tarea2/delangmedical/src/test/java/delangmedical/delangmedical/DelangmedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package delangmedical.delangmedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelangmedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
