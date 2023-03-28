329e3aadf45aa424bcae5b92385d80d883240ff7
AndresFLK <andrescrptycol@gmail.com>
2023-03-28 17:46:03 -0600

    Semana 11

--- tienda_l/src/main/java/com/tienda_l/SecurityConfig.java
+++ tienda_l/src/main/java/com/tienda_l/SecurityConfig.java
@@ -2,36 +2,73 @@ package com.tienda_l;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService users() {
-        UserDetails admin = User.builder()
-                .username("Juan")
-                .password("{noop}Juan123")
-                .roles("ADMIN", "VENDEDOR")
-                .build();
-        UserDetails sales = User.builder()
-                .username("Rebeca")
-                .password("{noop}Rebeca123")
-                .roles("ADMIN", "VENDEDOR")
-                .build();
-        UserDetails user = User.builder()
-                .username("Pedro")
-                .password("{noop}Pedro123")
-                .roles("USER")
-                .build();
+        @Bean
+        public UserDetailsService users() {
+                UserDetails admin = User.builder()
+                                .username("Juan")
+                                .password("{noop}Juan123")
+                                .roles("ADMIN", "VENDEDOR", "USER")
+                                .build();
+                UserDetails sales = User.builder()
+                                .username("Rebeca")
+                                .password("{noop}Rebeca123")
+                                .roles("VENDEDOR", "USER")
+                                .build();
+                UserDetails user = User.builder()
+                                .username("Pedro")
+                                .password("{noop}Pedro123")
+                                .roles("USER")
+                                .build();
 
-        return new InMemoryUserDetailsManager(user, sales, admin);
-    }
+                return new InMemoryUserDetailsManager(user, sales, admin);
+        }
 
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http)
+                        throws Exception {
+                http
+                                .authorizeHttpRequests((request) -> request
+                                                .requestMatchers("/",
+                                                                "/index",
+                                                                "/errores/**",
+                                                                "/webjars/**")
+                                                .permitAll()
+                                                .requestMatchers(
+                                                                "/articulo/nuevo",
+                                                                "/articulo/guardar",
+                                                                "/articulo/modificar/**",
+                                                                "/articulo/eliminar/**",
+                                                                "/categoria/nuevo",
+                                                                "/categoria/guardar",
+                                                                "/categoria/modificar/**",
+                                                                "/categoria/eliminar/**",
+                                                                "/cliente/nuevo",
+                                                                "/cliente/guardar",
+                                                                "/cliente/modificar/**",
+                                                                "/cliente/eliminar/**")
+                                                .hasRole("ADMIN")
+                                                .requestMatchers(
+                                                                "/articulo/listado",
+                                                                "/categoria/listado",
+                                                                "/cliente/listado")
+                                                .hasAnyRole("ADMIN", "VENDEDOR"))
+                                .formLogin((form) -> form
+                                                .loginPage("/login").permitAll())
+                                .logout((logout) -> logout.permitAll())
+                                .exceptionHandling()
+                                .accessDeniedPage("/errores/403");
+                return http.build();
+        }
 }
---
+++ tienda_l/src/main/java/com/tienda_l/Services/usuarioServices.java
@@ -0,0 +1,42 @@
+package com.tienda_l.Services;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.tienda_l.dao.UsuarioDao;
+import com.tienda_l.domain.Rol;
+import com.tienda_l.domain.Usuario;
+
+@Service("userDetailsService")
+public class usuarioServices implements UserDetailsService {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioDao.findByUsername(username);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        var roles = new ArrayList<GrantedAuthority>();
+
+        for (Rol rol : usuario.getRoles()) {
+            roles.add(new SimpleGrantedAuthority(rol.getNombre()));
+        }
+
+        return new User(usuario.getUsername(), usuario.getPassowrd(), roles);
+    }
+
+}
